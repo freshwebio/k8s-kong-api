4ec94ccbdf5e83a0a965abe860f723f7e5f21476
Andre Sutherland <andrelsf95@gmail.com>
2017-03-12 16:09:10 +0000

    Corrected json encoding annotations to omitempty to prevent current errors occuring because of empty ID fields.

--- kong/types.go
+++ kong/types.go
@@ -3,29 +3,29 @@ package kong
 // API represents a subset of the kong API object
 // which provide the properties this integration utilises.
 type API struct {
-	ID          string   `json:"id,optional"`
+	ID          string   `json:"id,omitempty"`
 	Name        string   `json:"name"`
 	URIs        []string `json:"hosts"`
 	UpstreamURL string   `json:"upstream_url"`
-	StripURI    bool     `json:"strip_uri,optional"`
+	StripURI    bool     `json:"strip_uri,omitempty"`
 }
 
 // Upstream provides a subset of the kong Upstream object.
 // We only care about the name, maybe in the future it will be worth supporting
 // the other properties.
 type Upstream struct {
-	ID   string `json:"id,optional"`
+	ID   string `json:"id,omitempty"`
 	Name string `json:"name"`
 }
 
 // Target provides the kong Target object
 // to be used in upstreams.
 type Target struct {
-	ID         string `json:"id,optional"`
+	ID         string `json:"id,omitempty"`
 	Target     string `json:"target"`
 	Weight     int    `json:"weight"`
-	UpstreamID string `json:"upstream_id,optional"`
-	Created    int    `json:"created_at,optional"`
+	UpstreamID string `json:"upstream_id,omitempty"`
+	Created    int    `json:"created_at,omitempty"`
 }
 
 // TargetList provides the data structure
